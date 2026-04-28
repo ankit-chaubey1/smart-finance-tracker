9c105a5e5b269743aa9ca941b829bd6feeea99b6
Ankit-chaubey1 <chaubeyankit927@gmail.com>
2026-04-28 12:04:59 +0530

    Restoring core project files

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
+
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth, connectAuthEmulator, setPersistence, browserLocalPersistence } from '@angular/fire/auth';
 import { provideDatabase, getDatabase } from '@angular/fire/database';
